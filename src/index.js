13423ae605622abdffd0c166111702447370bf9d
Rajeev Tiwari <rajeevtiwari30305gmail.com>
2025-01-01 16:13:01 +0530

    Utility like asyncHandler , Apierror , apiresponse

--- src/index.js
+++ src/index.js
@@ -2,13 +2,46 @@
  
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
 // Connect to MongoDB
-connectDB();
+connectDB()
+.then(() =>{
+  app.on('error' , (error)=>{
+      console.log("Error in starting the server : " , error); 
+      throw error
+  })
+  app.listen(process.env.PORT || 8000 , () =>{
+    console.log(`Server is Running at port : ${process.env.PORT} `)
+  } )
+
+})
+.catch((err) =>{
+  console.log("DB connection error !!! " , err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import express from "express";
 // const app = express()
 // ( async () =>{
